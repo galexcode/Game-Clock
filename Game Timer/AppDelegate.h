cd17121dd14843f4674cf07d21b7e05a2d6a250d
Josh Guffin <guffin@guffin.info>
2012-03-20 21:21:27 -0400

    Initial Commit

---
+++ Game Timer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Game Timer
+//
+//  Created by Josh Guffin on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Game Timer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Game Timer
+//
+//  Created by Josh Guffin on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
